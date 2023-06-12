6c9d698c02f333bfa21684d288b33b871d42bd62
Tam's Pizzeria <tampizzeria@gmail.com>
2023-06-12 20:48:21 +0200

    Regular auto-commit at 20:48:21 on 12/06/2023

---
+++ commit.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+cd D:\WEBSERVER\htdocs\articles
+git stash
+git pull
+sleep 3
+git stash pop
+git stash clear
+git add --all
+timestamp() {
+  date +"at %H:%M:%S on %d/%m/%Y"
+}
+git commit -am "Regular auto-commit $(timestamp)"
+git push origin master
+sleep 3s
+git fetch origin
+git reset --hard origin/master
+git pull
